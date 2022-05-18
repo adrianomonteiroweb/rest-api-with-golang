641df56d0bbdf2be10e08d07e9939dd37a8b1fbf
Adriano Monteiro <adrianomonteiroweb@mail.com>
2022-05-17 23:27:29 -0300

    NEW: server configurations.

--- main.go
+++ main.go
@@ -1,13 +1,22 @@
 package main
 
+import (
+	"github.com/gin-gonic/gin"
+)
 type todo struct {
-	ID					string
-	Item 				string
-	Completed		bool
+	ID					string `json:"id`
+	Item 				string `json:"title`
+	Completed		bool   `json:"complete`
 }
 
 var todos = []todo{
 	{ID: "1", Item: "Clean Room", Completed: false},
 	{ID: "2", Item: "Read Book", Completed: false},
 	{ID: "3", Item: "Record Video", Completed: false},
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/todos")
+	router.Run("localhost:9090")
 }
